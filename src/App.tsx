8acc1d1748c4a1c3dcf43dd18150094f1f7c876b
Harrison Kim <kim.harr@northeastern.edu>
2022-12-22 14:05:41 -0500

    navbar template

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import Feed from './components/Feed';
 import VideoDetail from './components/VideoDetail';
 import ChannelDetail from './components/ChannelDetail';
 import SearchFeed from './components/SearchFeed';
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: '#000' }}>
-        Navbar 
+        <Navbar /> 
         <Routes>
           <Route path="/" element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetail />} />
